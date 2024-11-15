461135669767d0bc4ab53c10b7cae0ff72728064
Willinghan Tomaz <willslaqtomaz@gmail.com>
2024-11-14 22:37:10 -0300

    chore: types fix

--- host/src/@types/dsl.d.ts
+++ host/src/@types/dsl.d.ts
@@ -1,6 +1,10 @@
 declare module "dsl/Button" {
   import { FC } from "react";
 
-  const Button: FC;
+  interface ButtonProps {
+    label: string;
+  }
+
+  const Button: FC<ButtonProps>;
   export default Button;
 }
--- host/src/App.tsx
+++ host/src/App.tsx
@@ -10,7 +10,7 @@ const App = () => (
     <div>Framework: react</div>
     <div>Language: TypeScript</div>
     <div>CSS: Tailwind</div>
-    <Button />
+    <Button label="Host MFE" />
   </div>
 );
 const rootElement = document.getElementById("app");
